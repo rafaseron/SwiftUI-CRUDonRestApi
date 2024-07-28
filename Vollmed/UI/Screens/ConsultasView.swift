3c48147e43529841a97e2fb46a29f5dea79d0e4e
rafaseron <rafaelseron3011@gmail.com>
2024-07-28 05:58:24 -0400

    Implementando consumo da requisicao na ConsultasView

--- Vollmed/UI/Screens/ConsultasView.swift
+++ Vollmed/UI/Screens/ConsultasView.swift
@@ -8,12 +8,45 @@
 import SwiftUI
 
 struct ConsultasView: View {
+    let service = WebService()
+    @State var appointments: [Appointment] = []
+    
     var body: some View {
-        VStack{
-            Text("ConsultasView")
-        }.navigationTitle("Minhas consultas")
-            .navigationBarTitleDisplayMode(.large)
+        ScrollView(showsIndicators: false){
+            LazyVStack{
+                ForEach(appointments){ appointment in
+                    SpecialistCardView(specialist: appointment.specialist)
+                }
+            }
+        }.onAppear{
+            //code
+        }
+        .task{
+            await getAllAppointmentsForThisPatient()
+        }
+        .navigationTitle("Agendadas")
+        .navigationBarTitleDisplayMode(.large)
+        
+        
     }
+    
+    // MARK: - Escopo dentro da struct ConsultasView
+    
+    func getAllAppointmentsForThisPatient() async {
+        do{
+            guard let appointmentList = try await service.getAppointmentsByPatientId(idPaciente: service.patientId) else{
+                return
+            }
+            
+            appointments = appointmentList
+            
+        }
+        catch{
+            print("Erro GET consultas by Id Paciente -> \(error)")
+        }
+    }
+    
+    
 }
 
 #Preview {
